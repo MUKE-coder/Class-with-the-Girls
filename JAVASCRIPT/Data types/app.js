74e8490b71d79aa0f31c88722b4fe75070613b37
MUKE-coder <gmukejohnbaptist@gmail.com>
2023-08-17 14:27:12 +0300

    added js

---
+++ JAVASCRIPT/Data types/app.js
@@ -0,0 +1,16 @@
+const height = 60;
+const color = "red";
+const gender = "female";
+
+// alert("Its working from app.js");
+// const answer = confirm("Are you sure want to leave this page");
+// console.log(answer);
+const username = prompt("What is your name?");
+const age = prompt("What is your age?");
+// const message = "Your name is"+ " " + username +" ";
+const message = `Your name is ${username} and your age is ${age}`;
+alert(message);
+const firstName = "Kinene";
+const lastName = "Joshua";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
